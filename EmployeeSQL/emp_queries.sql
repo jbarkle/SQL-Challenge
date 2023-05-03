8360ac932e7511c5ec408a5e57710cdea5d23436
jbarkle <jbarkle4@kent.edu>
2023-05-03 13:52:13 -0700

    wrote second query

--- EmployeeSQL/emp_queries.sql
+++ EmployeeSQL/emp_queries.sql
@@ -5,3 +5,10 @@ FROM employee em
 INNER JOIN salary sa
 ON em.emp_no = sa.emp_no;
 
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+
+SELECT first_name, last_name, hire_date
+FROM employee
+WHERE hire_date
+BETWEEN '1986-01-01' and '1986-01-31';
+
